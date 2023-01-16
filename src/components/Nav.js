a9b27553574ba1c305b835885e65ad37951a7abe
subhanaliweb <subhanali2020.sa@gmail.com>
2023-01-16 15:21:33 +0500

    scnd -rec

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,51 +1,110 @@
 import React from 'react'
-import styled from 'styled-components'
+import { NavLink } from 'react-router-dom';
+import styled from 'styled-components';
+import { FiShoppingCart } from 'react-icons/fi';
 
 const Nav = () => {
 
     const Nav = styled.nav`
-    .navbar-list {
+    .navbar-lists {
         display: flex;
         gap: 4.8rem;
-        li {
-          list-style: none;
-          .navbar-link {
-            &:link,
-            &:visited {
-              display: inline-block;
-              text-decoration: none;
-              font-size: 1.8rem;
-              text-transform: uppercase;
-              color: ${({ theme }) => theme.colors.black};
-              transition: color 0.3s linear;
-            }
-            &:hover,
-            &:active {
-              color: ${({ theme }) => theme.colors.helper};
-            }
+        align-items: center;
+
+        .navbar-link {
+          &:link,
+          &:visited {
+            display:inline-block;
+            text-decoration:none;
+            font-size: 1.8rem;
+            font-weight:500;
+            text-transform:uppercase;
+            color:${({theme})=> theme.colors.black};
+            transition:color 0.3s linear;
+          }
+
+          &:hover,
+          &:active {
+            color: ${({ theme }) => theme.colors.helper};
+            cursor:pointer;
           }
         }
       }
+
       .mobile-navbar-btn {
         display: none;
-        .close-outline {
-          display: none;
-        }
+        background-color: transparent;
+        cursor:pointer;
+        border:none;
       }
-      .mobile-navbar-btn[name="close-outline"] {
+      .mobile-nav-icon[name="close-outline"] {
         display: none;
       }
+      .close-outline {
+        display: none;
+      }
+
+      .cart-trolley--link {
+        position: relative;
+
+        .cart-trolley {
+          position: relative;
+          font-size: 3.2rem;
+        }
+
+        .cart-total--item{
+          width: 2.4rem;
+          height: 2.4rem;
+          position: absolute;
+          background-color:#000;
+          color: #000;
+          border-radius: 50%;
+          display:grid;
+          place-items : center;
+          top: -20%;
+          left: 70%;
+          background-color: ${({theme})=> theme.colors.helper};
+        }
+      }
+
+      .user-login--name{
+        text-transform:capitalize;
+      }
+
+      .user-logout,
+      .user-login{
+        font-size:1.4rem;
+        padding:0.8rem 1.4rem;
+      }
+
       @media (max-width: ${({ theme }) => theme.media.mobile}) {
         .mobile-navbar-btn {
           display: inline-block;
-          z-index: 999;
+          z-index: 9999;
           border: ${({ theme }) => theme.colors.black};
+
           .mobile-nav-icon {
             font-size: 4.2rem;
             color: ${({ theme }) => theme.colors.black};
           }
         }
+
+        .active .mobile-nav-icon {
+          display: none;
+          font-size: 4.2rem;
+          position: absolute;
+          top: 30%;
+          right: 10%;
+          color: ${({ theme }) => theme.colors.black};
+          z-index: 9999;
+        }
+
+        .active .close-outline {
+          display: inline-block;
+        }
+
         /* hide the original nav menu  */
+
         .navbar-list {
           width: 100vw;
           height: 100vh;
@@ -57,46 +116,81 @@ const Nav = () => {
           justify-content: center;
           align-content: center;
           flex-direction: column;
-          text-align: center;
+          // text-align: center;
           transform: translateX(100%);
           visibility: hidden;
           opacity: 0;
-          li {
-            .navbar-link {
-              &:link,
-              &:visited {
-                font-size: 4.2rem;
-              }
-              &:hover,
-              &:active {
-                color: ${({ theme }) => theme.colors.helper};
-              }
-            }
-          }
+          transition: all 3s linear;
         }
-        .active .mobile-nav-icon {
-          display: none;
-          font-size: 4.2rem;
-          position: absolute;
-          top: 3%;
-          right: 10%;
-          color: ${({ theme }) => theme.colors.black};
-          z-index: 9999;
-        }
-        .active .close-outline {
-          display: inline-block;
-        }
+
         .active .navbar-list {
           visibility: visible;
           opacity: 1;
           transform: translateX(0);
           z-index: 999;
+          transform-origin:right;
+          transition: all 3s linear;
+
+          .navbar-link {
+              font-size: 4.2rem;
+            }
+          }
+
+          .cart-trolley--link {
+            position:relative;
+
+          .cart-trolley {
+            position:relative;
+            font-size:5.2rem;
+          }
+
+          .cart-total--item {
+            width:4.2rem;
+            height:4.2rem;
+            font-size:2rem;
+          }
+        }
+
+        .user-logout,
+        .user-login {
+          font-size:2.2rem;
+          padding:0.8rem 1.4rem;
         }
       }
     `;
+
   return (
     <Nav>
-      Nav
+      <div className='navbar'>
+        <ul className='navbar-lists'>
+          <li>
+            <NavLink to='/' className='navbar-link home-link'>
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to='/about' className='navbar-link'>
+              About
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to='/products' className='navbar-link'>
+              Products
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to='/contact' className='navbar-link'>
+              Contact
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to='/cart' className='navbar-link cart-trolley--link'>
+              <FiShoppingCart className='cart-trolley'/>
+              <span className='cart-total--item'>10</span>
+            </NavLink>
+          </li>
+        </ul>
+      </div>
     </Nav>
   )
 }

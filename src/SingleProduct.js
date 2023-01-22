730ff7224221408bec0ab59eb315900185fd785b
subhanaliweb <subhanali2020.sa@gmail.com>
2023-01-23 04:35:19 +0500

    1/22/2023

--- src/SingleProduct.js
+++ src/SingleProduct.js
@@ -1,22 +1,84 @@
 import styled from "styled-components";
-import { Params, useParams } from "react-router";
+import { useParams } from "react-router";
 import { useEffect } from "react";
 import { useProductContext } from "./context/productscontext";
+import PageNavigation from "./components/PageNavigation";
+import MyImage from "./components/MyImage";
+import { Container } from "./styles/Container";
+import FormatPrice from "./helpers/FormatPrice"
+import { TbReplace, TbTruckDelivery } from 'react-icons/tb';
+import { MdSecurity } from 'react-icons/md';
+
 
 const API = `https://api.pujakaitem.com/api/products`;
 const SingleProduct = () =>{
 
 const { getSingleProduct, isSingleLoading, singleProduct } = useProductContext();
 const { id } = useParams();
-const { id: alias, name, company, price, description, stock, category, stars, reviews } = singleProduct;
+const { id: alias, name, company, price, description, stock, category, stars, reviews, image } = singleProduct;
 useEffect(() => {
   getSingleProduct(`${API}?id=${id}`);
 },[]);
 
+if(isSingleLoading){
+return <div className="page_loading">Loading.....</div>
+}
+
 return (
   <>
     <Wrapper>
-      <h1>Single Product {name}</h1>
+      <PageNavigation title={name} />
+      <Container className='container'>
+        <div className="grid grid-two-column">
+          <div className="product_images">
+          <MyImage imgs={image} />
+          </div>
+          <div className="product-data">
+            <h2>{name}</h2>
+            <p>{stars}</p>
+            <p>{reviews}</p>
+            <p className="product-data-price">MRP:
+            <del>
+              <FormatPrice price={price + 250000} />
+            </del>
+            </p>
+            <p className="product-data-price product-data-real-price">
+              Deal of the Day: <FormatPrice price={price} />
+            </p>
+            <p>{description}</p>
+            <div className="product-data-warranty">
+              <div className="product-warranty-data">
+                <TbTruckDelivery className="warranty-icon" />   
+                <p>free Delivery</p>   
+              </div>
+              <div className="product-warranty-data">
+                <TbReplace className="warranty-icon" />   
+                <p>30 Days Replacement</p>   
+              </div>
+              <div className="product-warranty-data">
+                <TbTruckDelivery className="warranty-icon" />   
+                <p>Product Delivered</p>   
+              </div>
+              <div className="product-warranty-data">
+                <MdSecurity className="warranty-icon" />   
+                <p>2 Year Warranty</p>   
+              </div>
+            </div>
+            <div className="product-data-info">
+              <p>
+                Available: 
+                <span>{stock > 0 ? "In Stock" : "Not Available"}</span>
+              </p>
+              <p>
+                ID: <span> {id} </span>
+              </p>
+              <p>
+                Brand: <span> {company} </span>
+              </p>
+            </div>
+          </div>
+        </div>
+      </Container>
     </Wrapper>
   </>
 )
---
+++ src/components/MyImage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const MyImage = () => {
+  return (
+    <div>
+      <h4>
+        My Image
+      </h4>
+    </div>
+  )
+}
+
+export default MyImage
